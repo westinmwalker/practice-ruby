f5a0a2e2962775263d01a083949b27ab101ec0d1
Westin Walker <westinwalker1988@gmail.com>
2023-06-27 13:01:49 -0600

    adds employee data as a hash

--- employee.rb
+++ employee.rb
@@ -2,3 +2,4 @@ employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 800
 employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
 
 puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} dollars a year."
+puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} dollars a year."

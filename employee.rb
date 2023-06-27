0274237844960375c15bc66bf29f6d05fc637e04
Westin Walker <westinwalker1988@gmail.com>
2023-06-27 12:59:15 -0600

    adds employee data as a hash

---
+++ employee.rb
@@ -0,0 +1,2 @@
+employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true }
+employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }

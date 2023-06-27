3cfd8778fb43daa369ae9af08ab8b6559527b543
Westin Walker <westinwalker1988@gmail.com>
2023-06-27 13:25:35 -0600

    adds shop data for items

--- employee.rb
+++ employee.rb
@@ -1,5 +1,1 @@
-employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true }
-employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
 
-puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} dollars a year."
-puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} dollars a year."
---
+++ store_item.rb
@@ -0,0 +1,7 @@
+# Shop items
+
+shirt = { color: "Red", size: "Large", price: 15 }
+jeans = { color: "Blue", size: "Extra Long", price: 40 }
+shoes = { color: "White", size: "12 Wide", price: 33 }
+
+puts "#{shirt[:color]} costs #{shirt[:price]} dollars."

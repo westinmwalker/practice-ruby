97dc519a0c65d92e658e1f7dc5e0088d26ed2c65
Westin Walker <westinwalker1988@gmail.com>
2023-06-27 13:26:53 -0600

    adds shop data for items

--- store_item.rb
+++ store_item.rb
@@ -4,4 +4,4 @@ shirt = { color: "Red", size: "Large", price: 15 }
 jeans = { color: "Blue", size: "Extra Long", price: 40 }
 shoes = { color: "White", size: "12 Wide", price: 33 }
 
-puts "#{shirt[:color]} costs #{shirt[:price]} dollars."
+puts "A #{shirt[:color]} shirt costs #{shirt[:price]} dollars."

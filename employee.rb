6394cf9f67797c3fcaf190a717c86b35881cbb02
Westin Walker <westinwalker1988@gmail.com>
2023-06-28 10:30:46 -0600

    sets up employee class

--- employee.rb
+++ employee.rb
@@ -1,9 +1,25 @@
+#Represents employee data as a class
 class Employee
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
     @active = input_active
+  end
+
+  #getter method returns first_name:
+  def first_name
+    @first_name
+  end
+
+  #getter method returns last_name:
+  def last_name
+    @last_name
+  end
+
+  #getter method returns active_status:
+  def active
+    @active
   end
 
   def print_info
@@ -21,3 +37,6 @@ employee1.print_info
 employee2.print_info
 employee1.give_annual_raise
 employee1.print_info
+p employee1.first_name
+p employee1.last_name
+p employee1.active

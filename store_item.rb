4212c14c8439e195a4770e86549014bd78e0888f
Westin Walker <westinwalker1988@gmail.com>
2023-06-28 10:52:46 -0600

    sets up shop items as class

--- store_item.rb
+++ store_item.rb
@@ -1,9 +1,32 @@
-# Shop items
+#Shop items represented as a class:
+class Shop_Item
+  def initialize(item_type, item_color, item_size, item_price)
+    @item_type = item_type
+    @item_color = item_color
+    @item_price = item_price
+    @item_size = item_size
+  end
 
-shirt = { color: "red", size: "large", price: 15 }
-jeans = { color: "blue", size: "extra long", price: 40 }
-shoes = { color: "white", size: "12 wide", price: 33 }
+  def item_color
+    @item_color
+  end
 
-puts "A #{shirt[:size]} #{shirt[:color]} shirt costs #{shirt[:price]} dollars."
-puts "A pair of #{jeans[:size]} #{jeans[:color]} jeans costs #{jeans[:price]} dollars."
-puts "A pair of #{shoes[:size]} #{shoes[:color]} shoes costs #{shoes[:price]} dollars."
+  def item_price
+    @item_price
+  end
+
+  def item_size
+    @item_size
+  end
+
+  def item_type
+    @item_type
+  end
+
+  def print_info
+    puts "The #{item_size} #{item_color} #{item_type} costs #{item_price} dollars."
+  end
+end
+
+item1 = Shop_Item.new("shirt", "red", "large", 15)
+item1.print_info

3df04df6fd3616dcaee03a902862f457ee1832c8
Westin Walker <westinwalker1988@gmail.com>
2023-06-28 10:24:28 -0600

    sets up employee class

--- employee.rb
+++ employee.rb
@@ -1,1 +1,23 @@
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
 
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} dollars a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info
+employee2.print_info
+employee1.give_annual_raise
+employee1.print_info

c231ce92337b46ded08be93580a5f8398cad40a1
Westin Walker <westinwalker1988@gmail.com>
2023-06-28 12:03:54 -0600

    uses single option hash for initialize method

--- store_item.rb
+++ store_item.rb
@@ -2,11 +2,11 @@
 class Item
   attr_accessor :item_color, :item_price, :item_size, :item_type
 
-  def initialize(item_type, item_color, item_size, item_price)
-    @item_type = item_type
-    @item_color = item_color
-    @item_price = item_price
-    @item_size = item_size
+  def initialize(item_options)
+    @item_type = item_options[:item_type]
+    @item_color = item_options[:item_color]
+    @item_price = item_options[:item_price]
+    @item_size = item_options[:item_size]
   end
 
   def print_info
@@ -14,15 +14,15 @@ class Item
   end
 end
 
-item1 = Item.new("shirt", "red", "large", 15)
-item2 = Item.new("jeans", "blue", "skinny", 40)
-item3 = Item.new("shoes", "white", "12 extra wide", 35)
+item1 = Item.new({ item_type: "shirt", item_color: "red", item_size: "large", item_price: 15 })
+item2 = Item.new({ item_type: "jeans", item_color: "blue", item_size: "skinny", item_price: 45 })
+item3 = Item.new({ item_type: "shoes", item_color: "white", item_size: "12 extra wide", item_price: 30 })
 item1.print_info
 item2.print_info
 item3.print_info
-item1.item_color = "black"
-item2.item_color = "khaki"
+item1.item_size = "medium"
 item2.item_type = "slacks"
+item2.item_color = "khaki"
 item3.item_price = 50
 item1.print_info
 item2.print_info

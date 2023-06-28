84dcfb6759fe9ac337c4a64fb8b06c035661d480
Westin Walker <westinwalker1988@gmail.com>
2023-06-28 11:35:11 -0600

    uses attribute reader and writer for shop item class

--- store_item.rb
+++ store_item.rb
@@ -1,26 +1,13 @@
 #Shop items represented as a class:
 class Shop_Item
+  attr_reader :item_color, :item_price, :item_size, :item_type
+  attr_writer :item_color, :item_price, :item_size, :item_type
+
   def initialize(item_type, item_color, item_size, item_price)
     @item_type = item_type
     @item_color = item_color
     @item_price = item_price
     @item_size = item_size
-  end
-
-  def item_color
-    @item_color
-  end
-
-  def item_price
-    @item_price
-  end
-
-  def item_size
-    @item_size
-  end
-
-  def item_type
-    @item_type
   end
 
   def print_info
@@ -31,6 +18,13 @@ end
 item1 = Shop_Item.new("shirt", "red", "large", 15)
 item2 = Shop_Item.new("jeans", "blue", "skinny", 40)
 item3 = Shop_Item.new("shoes", "white", "12 extra wide", 35)
+item1.print_info
+item2.print_info
+item3.print_info
+item1.item_color = "black"
+item2.item_color = "khaki"
+item2.item_type = "slacks"
+item3.item_price = 50
 item1.print_info
 item2.print_info
 item3.print_info
